920fa95caae311d4804769a2b43c12a4253e341c
celisej567 <celisej567@gmail.com>
2026-01-13 19:57:54 +0300

    Initial commit

---
+++ launch/drazdropermovich.cpp
@@ -0,0 +1,60 @@
+#include "iostream"
+#include "SDL3/SDL.h"
+#include "glad/glad.h"
+#include "GL/gl.h"
+
+#define WND_WIDTH 800
+#define WND_HEIGHT 600
+
+int main()
+{
+    puts("bebra\n");
+ 
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* wnd = SDL_CreateWindow("launch", 800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+    SDL_Renderer* ren = SDL_CreateRenderer(wnd, NULL);
+
+    SDL_GLContext sdl_gl = SDL_GL_CreateContext(wnd);
+
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0,0,WND_WIDTH,WND_HEIGHT);
+
+    SDL_Event event;
+
+    bool quit = false;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_EVENT_QUIT:
+                    quit = true;
+                    break;
+            }
+        }
+
+        SDL_RenderClear(ren);
+
+        SDL_RenderPresent(ren);
+
+    }
+    
+    if(!SDL_GL_DestroyContext(sdl_gl))
+    {
+        std::cout << "Failed to destroy context: \n" <<  SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(wnd);
+
+    puts("bebra2\n");
+    return 0;
+}
